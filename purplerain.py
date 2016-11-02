a267011995be9294d204fe1a77c816d9ed4c4eff
Os Sutrisno <oswinx@gmail.com>
2016-11-03 01:30:12 +0800

    Update purplerain.py

--- purplerain.py
+++ purplerain.py
@@ -1,14 +1,11 @@
 import requests
-#import base64
 import json
 import urllib 
-#import uuid
-#from PIL import Image
 import os
 import time
-#import numpy as np
-#import cv2
-import vlc
+#import vlc
+import sched
+import sqlite3
 
 #APIs
 face_recog_api = 'http://rrpp22.azurewebsites.net/api/video/process'
@@ -26,10 +23,6 @@ if not os.path.exists(asset_dir):
     print 'Created: ' + asset_dir
 
 print 'Assets folder set to: ' + asset_dir
-
-#default header
-header = {'content-type':'application/json'}
-
 
 def get_playlist():
     resp = requests.get(content_sync_api)
@@ -52,61 +45,93 @@ def download_file(source_url):
     except:
         print "file download error"
     return filename
-            
-#def run_playlist():
-print 'Getting content playlist from server.'
-current_playlist = get_playlist()
-print 'Done.'
 
-#TODO: spin off new thread to constantly check if playlist has changed. if changed, update VLC playlist and play new content.
-    #while True:
-new_playlist = get_playlist()
-if new_playlist != current_playlist:
-    current_playlist = new_playlist
+def create_playlist_table(conn, c):
+    c.execute("CREATE TABLE playlist (sequence int, filename text, path text)")
+    conn.commit()
 
-#print current_playlist['ContentList'][1]
-#print current_playlist['ContentList'][1]['ContentName']
+def set_base_content(conn, c):
+    c.execute("INSERT INTO playlist VALUES (1, 'male.jpg', '" + asset_dir + "male.jpg')")
+    c.execute("INSERT INTO playlist VALUES (2, 'female.jpg', '" + asset_dir + "female.jpg')")
+    conn.commit()
 
-vlc_instance = vlc.Instance()
-#add base media content -- content to display even if the sync fails
-print 'Setting up base content.'
-media = vlc_instance.media_new_path('/home/os/Code/python-scripts/assets/male.jpg') #to check if base content exists
-media_list = vlc_instance.media_list_new()
-media_list.add_media(media)
-media = vlc_instance.media_new_path('/home/os/Code/python-scripts/assets/female.jpg')
-media_list.add_media(media)
-#test video
-#media_list.add_media(vlc_instance.media_new('/home/os/Code/python-scripts/assets/movie.mp4'))
-print 'Done'
+def get_max_sequence(conn, c):
+    c.execute("SELECT MAX(sequence) FROM playlist")
+    results = c.fetchone()
+    max_seq = results[0]
+    print 'Current max sequence: ' + str(max_seq)
+    max_seq = max_seq + 1
+    print 'Incrementing to: ' + str(max_seq)
+    return max_seq
 
-print 'Setting up media player instances'
-mlplayer = vlc_instance.media_list_player_new()
-mlplayer.set_media_list(media_list)
+def write_playlist():
+    print 'Getting content playlist from server.'
+    current_playlist = get_playlist()
+    print 'Done.'
 
-mplayer = vlc_instance.media_player_new()
-mplayer.set_fullscreen(True)
-mlplayer.set_media_player(mplayer)
+    new_playlist = get_playlist()
+    if new_playlist != current_playlist:
+        current_playlist = new_playlist
 
-for item in current_playlist['ContentList']:
-    filename = download_file(item['ContentName'])
-    print item['ContentName'] + ' downloaded to ' + filename
-    media = vlc_instance.media_new_path(filename)
-    media_list.add_media(media)
+    print 'Connecting to database...'
+    conn = sqlite3.connect('purplepi.db')
+    c = conn.cursor()
+    print 'Connection opened.'
 
-mlplayer.play()
-time.sleep(60)
+    try:
+        c.execute("SELECT * FROM playlist")
+    except sqlite3.OperationalError:
+        print "Table playlist not found. Creating one."
+        create_playlist_table(conn, c)
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
+        raise
 
-'''image_files = [
-'/home/os/Code/python-scripts/assets/male.jpg',
-'/home/os/Code/python-scripts/assets/female.jpg'
-]
+    #print current_playlist['ContentList'][1]
+    #print current_playlist['ContentList'][1]['ContentName']
 
-for image in image_files:
-    img = cv2.imread(image, 1)
-    print 'before'
-    cv2.imshow('male.jpg', img)
-    print 'after'
-    cv2.waitKey(10000)
+    #Set base content into queue
+    print 'Setting up base content.....'
+    c.execute("SELECT COUNT(*) FROM playlist")
+    count = c.fetchone()
+    print count[0]
 
-cv2.destroyAllWindows()'''
+    if count[0] <= 0:
+        print 'Table empty - set base content'
+        set_base_content(conn, c)
+    
+    #Check bases content data
+    c.execute("SELECT * FROM playlist")
+    print c.fetchall()
+    print 'Done'
 
+    print 'Download contents...'
+    max_seq = get_max_sequence(conn, c)
+    for item in current_playlist['ContentList']:
+        filename = download_file(item['ContentName'])
+        print item['ContentName'] + ' downloaded to ' + filename
+        c.execute("INSERT INTO playlist VALUES (" + str(max_seq) + ", '"+ item['ContentName'] +"', '" + filename + "')")
+        max_seq += 1
+
+    conn.commit()
+    c.execute("SELECT * FROM playlist")
+    print c.fetchall()
+    print 'Done'
+
+def get_targeted_ad():
+    print 'getting targeted ad....'
+    default_snapshot_interval = 5 #5 seconds interval of each photos
+
+    #threading.Timer(default_snapshot_interval, get_targeted_ad)
+    
+    #take photo
+    #call API and get response
+    #add response to global queue
+
+    #repeat
+s = sched.scheduler(time.time, time.sleep)
+
+write_playlist()
+
+'thread 1 - get playlist, populate global queue, play items'
+'thread 2 - take pic, upload, get response and update global queu'
